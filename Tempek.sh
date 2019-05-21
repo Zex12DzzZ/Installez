0970714fd65b5d91e5faeb4c0cd13d86701d1887
FR05H <50860063+Zex12DzzZ@users.noreply.github.com>
2019-05-21 13:26:15 +0700

    Add files via upload

---
+++ Tempek.sh
@@ -0,0 +1,70 @@
+#!/system/xbin/bash
+#ngapain luh 
+#mau record 
+#awokawokawokawokawok 
+#gak buat luh jadi pro
+#jangan lupa chat saya
+#081376026952
+#IGnya juga Bennetto29 
+clear
+cd module
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 2
+python2 please.py
+echo $red
+cat Voss.txt
+echo
+echo 
+echo
+echo "" 
+echo "\033[32;1m<><><><><><>FR05H<><><><><>" |lolcat
+echo "\033[31;1mAuthor:\033[33;1mFR05H" 
+echo "\033[36;1mTeam : \033[033;2mSilent Ghost" 
+echo "\033[33;1mWA:\033[37;1m081376026952" 
+echo "" 
+echo "\033[33;1mPilih tools yg ingin di install" |lolcat
+echo "\033[31;1m"
+echo "[===============================================[>"
+echo $yellow "1.Install Red Hawk"
+sleep 1 
+echo "[===============================================[>" 
+sleep 1
+echo $red "0.Keluar"
+echo "[÷]============================================[÷]" 
+echo "\033[33;1m"
+read -p "Root@FR05H~#" tol
+if [ $tol = 1 ] || [ $tol = 01 ]
+then
+clear
+toilet -f standard -F gay "FR05H" 
+echo "\033[31;1mTekan Y bila perluh" 
+sleep 2
+apt update 
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $tol = 0 ] || [ $tol = 00 ] 
+then 
+clear 
+echo $red "See you" 
+clear 
+sleep 1
+echo $red "don't Forget follow my ig" 
+clear
+sleeo 1
+echo $red "Bennetto29" 
+echo "\033[32;1m082276026952" 
+exit
+fi
